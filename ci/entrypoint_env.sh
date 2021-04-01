9ffae34566a150455e2e47cd0c618200b3e0eb4a
Amitav Mitra <amitavm@bu.edu>
2021-04-01 13:28:41 -0400

    point to BUTool/ApolloSM libraries

--- ci/entrypoint_env.sh
+++ ci/entrypoint_env.sh
@@ -1,3 +1,2 @@
 export CACTUS_ROOT=/opt/cactus:$CACTUS_ROOT
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
-source ApolloHerd/env.sh
+export LD_LIBRARY_PATH=/usr/local/lib:/opt/BUTool/lib:$LD_LIBRARY_PATH
