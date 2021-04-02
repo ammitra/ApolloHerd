567c2dc5c12e1d2aec0451d2d60f8e7124f2fb6d
Amitav Mitra <amitavm@bu.edu>
2021-04-02 16:12:56 -0400

    rewrote CMPwrUp

--- src/common/commands/CMPwrUp.cpp
+++ src/common/commands/CMPwrUp.cpp
@@ -1,3 +1,10 @@
+/* 
+  ApolloSMDevice functionality:
+  https://github.com/apollo-lhc/ApolloSM_plugin/blob/develop/src/ApolloSM_device/ApolloSM_device.cc#L229
+
+  - ApolloSMDevice::CMPowerUp calls ApolloSM::PowerUpCM, which returns bool
+*/
+
 #include "swatch/apolloherd/commands/CMPwrUp.hpp"
 #include "swatch/apolloherd/ApolloDevice.hpp"
 #include <BUTool/CommandReturn.hh>
@@ -21,18 +28,36 @@ action::Command::State CMPwrUp::code(const core::ParameterSet& aParams)
 {
   ApolloDeviceController& lController = getActionable<ApolloDevice>().getController();
 
-  std::string CMID = aParams.get<std::string>("CM ID   ");
-  std::string WAIT = aParams.get<std::string>("wait (s)");
+  // gather command and arguments 
+  std::vector<std::string> cmpwrup_args {"cmpwrup"};
+  cmpwrup_args.push_back(aParams.get<std::string>("CM ID   "));
+  cmpwrup_args.push_back(aParams.get<std::string>("wait (s)"));
 
-  std::string CMID_WAIT;
-  CMID_WAIT = CMID + ' ' + WAIT;
+  State lState;
 
-  State lState = kDone;
+  // perform command
+  CommandReturn::status result = lController.EvaluateCommand(cmpwrup_args);
 
-  if (lController.cmpwrup(CMID_WAIT) == CommandReturn::status::BAD_ARGS)
-    throw core::RuntimeError("bad arguments");
-  else
-    lState = kDone;
+  // check the command result, send to HERD control console (BUTextIO functionality)
+  switch (result) {
+    case 0:
+      lState = kError;
+      setStatusMsg("Command not found");
+      break;
+    case 2:
+      lState = kError;
+      setStatusMsg("Bad arguments");
+      break;
+    case 1:
+      lState = kDone;
+      break;
+      /* 
+         at this point, we know the command was successfully executed, but don't know if CM actually pwrd up 
+         We'd need to capture the printf result from ApolloSM::PowerDownCM, which contains that information
+         this functionality would be implemented through BUTextIO, and could be sent to the HERD control console via setResult
+      */
+      //setResult( result from BUTextIO stringstream )
+  }
 
   return lState;
 }
