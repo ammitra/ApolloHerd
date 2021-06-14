06d4c08d6d1a3e67636722b94cad5f59063220de
Amitav Mitra <amitav.mitra@cern.ch>
2021-06-14 11:45:06 -0400

    pass ostringstream to BUTool::RegisterHelper to allow command result to be sent to shep

--- src/common/commands/ApolloAccess.cpp
+++ src/common/commands/ApolloAccess.cpp
@@ -22,14 +22,26 @@ action::Command::State ApolloAccess::code(const core::ParameterSet& aParams)
 {
   ApolloDeviceController& lController = getActionable<ApolloDevice>().getController();
 
-  State lState = kDone;
+  // initialize a return value object
+  State lState;
+  // gather command and arguments in a string to pass to dev_cmd
+  std::string params = aParams.get<std::string>("command_and_arguments");
+  // initialize a stringstream to capture command result
+  std::ostringstream oss;
+  // add this stream (via ApolloSMDevice functionality inherited from BUTextIO)
+  lController.AddStream(Level::INFO, &oss);
+  // perform the command, have output written to oss
 
-  if (lController.dev_cmd(aParams.get<std::string>("command_and_arguments")) == CommandReturn::status::NOT_FOUND)
+
+  if (lController.dev_cmd(params) == CommandReturn::status::NOT_FOUND)
     throw core::RuntimeError("command not found");
-  else if (lController.dev_cmd(aParams.get<std::string>("command_and_arguments")) == CommandReturn::status::BAD_ARGS)
+  else if (lController.dev_cmd(params) == CommandReturn::status::BAD_ARGS)
     throw core::RuntimeError("bad arguments");
   else
     lState = kDone;
+
+  // send the command result to the herd control console
+  setResult(oss.str());
 
   return lState;
 }
