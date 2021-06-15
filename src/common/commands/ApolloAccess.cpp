cf22cdcee74dba7b53a9c01ac95bd6b06e02511d
Amitav Mitra <amitav.mitra@cern.ch>
2021-06-15 18:44:47 -0400

    fix CommandReturn::status -> int comparison

--- src/common/commands/ApolloAccess.cpp
+++ src/common/commands/ApolloAccess.cpp
@@ -31,7 +31,7 @@ action::Command::State ApolloAccess::code(const core::ParameterSet& aParams)
   // add this stream (via ApolloSMDevice functionality inherited from BUTextIO)
   lController.AddStream(Level::INFO, &oss);
   // perform the command, have output written to oss
-  CommandReturn::status result = lController.dev_cmd(params);
+  int result = lController.dev_cmd(params);
 
   // check CommandReturn status
   if (result == CommandReturn::status::NOT_FOUND)
