0c9c249e57c7fd5e4609516fb45020b674cbd3f4
Amitav Mitra <amitavm@bu.edu>
2021-06-17 14:53:29 -0400

    revert changes

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -67,8 +67,6 @@ if [ "$1" != "app" ]; then
     # 5c) patch ApolloSM_plugin Makefile to deal with strange format-truncation error (append -Wno-format-trunctation to CXX_FLAGS variable)
     sed -i "s|-Wno-ignored-qualifiers|-Wno-ignored-qualifiers -Wno-format-truncation|g" plugins/ApolloSM_plugin/Makefile
     # time to build
-    export UHAL_VER_MAJOR=2
-    export UHAL_VER_MINOR=7
     make local -j$(nproc) RUNTIME_LDPATH=/opt/BUTool COMPILETIME_ROOT=--sysroot=/
     make install RUNTIME_LDPATH=/opt/BUTool COMPILETIME_ROOT=--sysroot=/ INSTALL_PATH=/opt/BUTool
     cd ..
