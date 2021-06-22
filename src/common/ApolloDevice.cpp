0ceed242462227504bb8eb46c3b6a61da8faaa57
Amitav Mitra <amitavm@bu.edu>
2021-06-22 16:38:14 -0400

    include program command

--- src/common/ApolloDevice.cpp
+++ src/common/ApolloDevice.cpp
@@ -1,5 +1,6 @@
 #include "swatch/apolloherd/ApolloDevice.hpp"
 #include "swatch/apolloherd/commands/ApolloAccess.hpp"
+#include "swatch/apolloherd/commands/Program.hpp"
 #include "swatch/core/Factory.hpp"
 #include "swatch/apolloherd/ApolloDeviceController.hpp"
 
