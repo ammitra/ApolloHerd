ee791ca579e921a60ddb5dd2a8f4d44116919018
Amitav Mitra <amitavm@bu.edu>
2021-06-28 10:29:33 -0400

    point to main ApolloTool repo

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -5,7 +5,7 @@ set -x
 
 UHAL_VERSION=2.8.1
 UIOUHAL_VERSION="develop"
-APOLLOTOOL_VERSION="uhal_versioning_change"
+APOLLOTOOL_VERSION="master"
 
 if [ "$1" != "app" ]; then
 
@@ -58,7 +58,7 @@ if [ "$1" != "app" ]; then
     yum clean all
 
     # 5) build BUTool from ApolloTool meta repository
-    git clone --branch ${APOLLOTOOL_VERSION} https://github.com/ammitra/ApolloTool.git
+    git clone --branch ${APOLLOTOOL_VERSION} https://github.com/apollo-lhc/ApolloTool.git
     cd ApolloTool
     make init
     # 5b) patch BUTool and plugins to use _GLIBCXX_USECXX11_ABI macro
