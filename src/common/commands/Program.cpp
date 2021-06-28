852eb0ab014eef9a63c4837f26e0a62ff19c8d3e
Amitav Mitra <amitavm@bu.edu>
2021-06-28 18:39:50 -0400

    rename reference

--- src/common/commands/Program.cpp
+++ src/common/commands/Program.cpp
@@ -49,7 +49,7 @@ action::Command::State Program::code(const core::ParameterSet& aParams)
   }
   setProgress(0.3, "Powering up CM" + CMID);
   // cmpwrup command will return success regardless of whether CM powered up -> need to add BUTextIO to ApolloSMDevice::CMPowerUp
-  ApolloCMFPGA.ApolloAccess("cmpwrup "+CMID); // use default wait time (1s)
+  ApolloCM.ApolloAccess("cmpwrup "+CMID); // use default wait time (1s)
 
   // 3) Program the FPGA (using svfplayer from ApolloSMDevice -> ApolloSM)
   SetProgress(0.6, "Programming CM" + CMID + " via svfplayer");
@@ -59,7 +59,7 @@ action::Command::State Program::code(const core::ParameterSet& aParams)
   std::string XVCLabel = aParams.get<std::string>("XVCLabel");
   std::string command_and_args = svfplayer + " " + svfFile + " " + XVCLabel;
   // pass svfplayer command and args to EvaluateCommand via ApolloAccess
-  int result = ApolloCMFPGA.ApolloAccess(command_and_args);
+  int result = ApolloCM.ApolloAccess(command_and_args);
 
   // 4) compare command result
   // probably need to add a few more here - will do after i understand how svfplayer works and what it returns
@@ -68,7 +68,7 @@ action::Command::State Program::code(const core::ParameterSet& aParams)
 
   // 5) Update address table used by EMP commands
   setProgress(0.9, "Updating address table");
-  ApolloCMFPGA.replaceController("file://" + lBuildProducts.addressTable);
+  ApolloCM.replaceController("file://" + lBuildProducts.addressTable);
 
 
   // 6) Read build metadata and run simple checks
